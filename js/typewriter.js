11f204e9e01dfdfab0c7e5795f175d28c6b5b244
DeniceDaCosta <ddacosta@noramndiewebschool.fr>
2020-03-19 11:11:02 +0100

    3

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
-    var dataText = [ "Designer", "Développer front-end"];
+    var dataText = [ "Designer", "Développeur front-end"];
     
     // type one text in the typwriter
     // keeps calling itself until the text is finished
